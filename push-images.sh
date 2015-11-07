6c18d0a0dd3e96a5441ef75c0ed617b221285e0b
Kelvin Chen <kelvin@kelvinchen.org>
2015-11-06 19:15:13 -0500

    Add script to push all images to dockerhub

---
+++ push-images.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Push all images to the docker registry.
+
+docker push kelvinchen/seedbox:base
+docker push kelvinchen/seedbox:frontend
+docker push kelvinchen/seedbox:plex
+docker push kelvinchen/seedbox:rtorrent
+docker push kelvinchen/seedbox:sickrage
+docker push kelvinchen/seedbox:syncthing
