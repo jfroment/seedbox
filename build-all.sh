aaf37c5d9e143e5acedb0a2a6fbd22ada4f3517c
Kelvin Chen <kelvin@kelvinchen.org>
2015-11-11 21:29:01 -0500

    Restyle Dockerfiles
    Use leading ampersands instead of trailing ampersands as per the
    examples on the docker website. e.g.
    long command \
    && another command
    
    Also change the build-all script to update the debian image when
    building the base image.

--- build-all.sh
+++ build-all.sh
@@ -5,10 +5,10 @@
 
 cd ${0%/*}
 
-docker build -t kelvinchen/seedbox:base      Dockerfiles/base
-docker build -t kelvinchen/seedbox:frontend  Dockerfiles/frontend
-docker build -t kelvinchen/seedbox:plex      Dockerfiles/plex
-docker build -t kelvinchen/seedbox:rtorrent  Dockerfiles/rtorrent
-docker build -t kelvinchen/seedbox:sickrage  Dockerfiles/sickrage
-docker build -t kelvinchen/seedbox:syncthing Dockerfiles/syncthing
-docker build -t kelvinchen/seedbox:openvpn   Dockerfiles/openvpn
+docker build -t kelvinchen/seedbox:base --pull Dockerfiles/base
+docker build -t kelvinchen/seedbox:frontend    Dockerfiles/frontend
+docker build -t kelvinchen/seedbox:plex        Dockerfiles/plex
+docker build -t kelvinchen/seedbox:rtorrent    Dockerfiles/rtorrent
+docker build -t kelvinchen/seedbox:sickrage    Dockerfiles/sickrage
+docker build -t kelvinchen/seedbox:syncthing   Dockerfiles/syncthing
+docker build -t kelvinchen/seedbox:openvpn     Dockerfiles/openvpn
