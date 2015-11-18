e2d72794d6dbd1b14b5bb082a7f07801db53c685
Kelvin Chen <kelvin@kelvinchen.org>
2015-11-18 17:30:30 -0500

    Update image building script
    - Change building to build images concurrently
    - Add flags to support some build options

--- build-all.sh
+++ build-all.sh
@@ -5,10 +5,30 @@
 
 cd ${0%/*}
 
+usage() {
+    echo "./build-all.sh [-n] [-q]"
+    echo "  -n Do not use cache when building docker images"
+    echo "  -q Quiet mode"
+    exit 1
+}
+
+ARGS=
+
+while getopts ":nq" opt; do
+    case $opt in
+        n) ARGS="$ARGS --no-cache" ;;
+        q) ARGS="$ARGS --quiet" ;;
+        \?) usage ;;
+    esac
+done
+
 docker build -t kelvinchen/seedbox:base --pull Dockerfiles/base
-docker build -t kelvinchen/seedbox:frontend    Dockerfiles/frontend
-docker build -t kelvinchen/seedbox:plex        Dockerfiles/plex
-docker build -t kelvinchen/seedbox:rtorrent    Dockerfiles/rtorrent
-docker build -t kelvinchen/seedbox:sickrage    Dockerfiles/sickrage
-docker build -t kelvinchen/seedbox:syncthing   Dockerfiles/syncthing
-docker build -t kelvinchen/seedbox:openvpn     Dockerfiles/openvpn
+
+docker build -t kelvinchen/seedbox:frontend  $ARGS dockerfiles/frontend  &
+docker build -t kelvinchen/seedbox:plex      $ARGS dockerfiles/plex      &
+docker build -t kelvinchen/seedbox:rtorrent  $ARGS dockerfiles/rtorrent  &
+docker build -t kelvinchen/seedbox:sickrage  $ARGS dockerfiles/sickrage  &
+docker build -t kelvinchen/seedbox:syncthing $ARGS dockerfiles/syncthing &
+docker build -t kelvinchen/seedbox:openvpn   $ARGS dockerfiles/openvpn   &
+
+wait
