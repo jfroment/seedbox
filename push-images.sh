2a41ff1cb5da42e0cd5529f6c27042957c090e5a
Kelvin Chen <kelvin@kelvinchen.org>
2016-01-26 17:07:12 -0500

    Make push script concurrent

--- push-images.sh
+++ push-images.sh
@@ -3,9 +3,11 @@
 # Push all images to the docker registry.
 
 docker push kelvinchen/seedbox:base
-docker push kelvinchen/seedbox:frontend
-docker push kelvinchen/seedbox:plex
-docker push kelvinchen/seedbox:rtorrent
-docker push kelvinchen/seedbox:sickrage
-docker push kelvinchen/seedbox:syncthing
-docker push kelvinchen/seedbox:openvpn
+docker push kelvinchen/seedbox:frontend &
+docker push kelvinchen/seedbox:plex &
+docker push kelvinchen/seedbox:rtorrent &
+docker push kelvinchen/seedbox:sickrage &
+docker push kelvinchen/seedbox:syncthing &
+docker push kelvinchen/seedbox:openvpn &
+
+wait
