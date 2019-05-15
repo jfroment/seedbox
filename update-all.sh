032ac51d372a507606814e238f2cb0325c5047c4
Jean Froment <jean.froment@apalia.net>
2019-05-15 09:27:18 +0200

    Improve update script to prune images

--- update-all.sh
+++ update-all.sh
@@ -3,6 +3,9 @@
 echo "[$0] ***** Pulling all images... *****"
 docker-compose pull
 echo "[$0] ***** Recreating containers if required... *****"
-docker-compose up -d
-echo "[$0] ***** Done. *****"
+docker-compose up -d --remove-orphans
+echo "[$0] ***** Done updating containers *****"
+echo "[$0] ***** Clean unused images... *****"
+docker image prune -af
+echo "[$0] ***** Done! *****"
 exit 0
