5f9e3c7a1d21506d7b746c0cd51e57fe2c2d5dde
Jean Froment <jean.froment@apalia.net>
2020-09-11 12:24:02 +0200

    Unmanage acme.json file

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "[$0] Initializing..."
-sudo mkdir /opt/traefik
-sudo touch /opt/traefik/acme.json && sudo chmod 600 /opt/traefik/acme.json
 cp .env.sample .env
 echo "[$0] Please edit .env file"
 exit 0
