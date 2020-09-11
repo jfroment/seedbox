930268b6c0d68a0e90f39686ca7d39e8332999b2
Jean Froment <jean.froment@apalia.net>
2020-09-11 13:12:59 +0200

    Fix path in file provider for common-auth middleware

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "[$0] Initializing..."
-cp .env.sample .env
-echo "[$0] Please edit .env file"
+docker network create traefik-network || true
+if [[ ! -f .env ]]; then
+  cp .env.sample .env
+  echo "[$0] Please edit .env file"
+fi
+echo "[$0] Done."
 exit 0
--- update-all.sh
+++ update-all.sh
@@ -3,7 +3,6 @@
 # Create/update http_auth file according to values in .env file
 source .env
 echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
-docker network create traefik-network || true
 
 echo "[$0] ***** Pulling all images... *****"
 docker-compose pull
