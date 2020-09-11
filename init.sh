593d24f9e10ef2431cd58b8846bc9859bc2e143c
Jean Froment <jean.froment@apalia.net>
2020-09-11 15:10:37 +0200

    Typo fix

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[$0] Initializing..."
-docker network create traefik-network 2&>1 || true
+docker network create traefik-network 2>&1 || true
 if [[ ! -f .env ]]; then
   cp .env.sample .env
   echo "[$0] Please edit .env file"
