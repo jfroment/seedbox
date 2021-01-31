74affa6dae97b133a0634e5fc14cf16ceb9873a4
Jean Froment <jean.froment@apalia.net>
2021-01-31 18:09:33 +0100

    Split docker-compose.yml into multiple files in dedicated dir

--- update-all.sh
+++ update-all.sh
@@ -4,10 +4,14 @@
 source .env
 echo "${HTTP_USER}:${HTTP_PASSWORD}" > traefik/http_auth
 
+# Fetch all YAML files
+SERVICES=$(find services2 -name "*.yaml" -mindepth 1 -maxdepth 1 | sed -e 's/^/-f /')
+ALL_SERVICES="-f docker-compose.yaml $SERVICES"
+
 echo "[$0] ***** Pulling all images... *****"
-docker-compose pull
+docker-compose ${ALL_SERVICES} pull
 echo "[$0] ***** Recreating containers if required... *****"
-docker-compose up -d --remove-orphans
+docker-compose ${ALL_SERVICES} up -d --remove-orphans
 echo "[$0] ***** Done updating containers *****"
 echo "[$0] ***** Clean unused images... *****"
 docker image prune -af
